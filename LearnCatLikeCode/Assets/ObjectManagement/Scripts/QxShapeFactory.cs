fe5068f501c79030b3e586bb1b5ed3c24ddd8c65
LUFFY <bbc724098@hotmail.com>
2022-10-18 23:53:14 +0800

    暂存添加shape

---
+++ LearnCatLikeCode/Assets/ObjectManagement/Scripts/QxShapeFactory.cs
@@ -0,0 +1,21 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QxShape", menuName = "QxShapes", order = 0)]
+public class QxShapeFactory : ScriptableObject
+{
+    [SerializeField]
+    QxShape[] Prefabs;
+
+    public QxShape Get(int shapeId)
+    {
+        return Instantiate(Prefabs[shapeId]);
+    }
+
+    public QxShape GetRandom()
+    {
+        return Get(Random.Range(0, Prefabs.Length));
+    }
+    
+    
+}
